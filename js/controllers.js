ecb8f008b07a896ba4b3993401989ce28f12a1f0
Eric Shi <eric.shi@convertlab.com>
2015-11-24 09:41:32 +0800

    add cordova plugin

--- js/controllers.js
+++ js/controllers.js
@@ -1,8 +1,29 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function ($scope,Sliders) {
+.controller('DashCtrl', function ($scope,Sliders, $cordovaGeolocation, $ionicLoading) {
     $scope.sliders = Sliders.all();
     $scope.chats = Sliders.chats();
+    document.addEventListener("deviceready", onDeviceReady, false);
+    function onDeviceReady() {
+    	alert("device ready");
+    	$ionicLoading.show({
+            template: '<ion-spinner icon="bubbles"></ion-spinner><br/>Acquiring location!'
+        });
+    	var posOptions = {
+                enableHighAccuracy: true,
+                timeout: 20000,
+                maximumAge: 0
+            };
+    	$cordovaGeolocation.getCurrentPosition(posOptions).then(function (position) {
+            var lat  = position.coords.latitude;
+            var long = position.coords.longitude;
+            alert(JSON.stringify(position));
+             
+        }, function(err) {
+            $ionicLoading.hide();
+            alert(JSON.stringify(err));
+        });
+    }
 })
 
 .controller('ChatsCtrl', function($scope, Chats) {
--- js/services.js
+++ js/services.js
@@ -95,29 +95,24 @@ angular.module('starter.services', [])
 
     var chats = [{
         id: 0,
-        name: 'Ben Sparrow',
-        lastText: 'You on your way?',
-        face: 'https://pbs.twimg.com/profile_images/514549811765211136/9SgAuHeY.png'
+        title: '【东京北海道】初夏花未央',
+        subTitle: '晓稀 ',
+        img: 'img/banners/1.jpg'
     }, {
         id: 1,
-        name: 'Max Lynx',
-        lastText: 'Hey, it\'s me',
-        face: 'https://avatars3.githubusercontent.com/u/11214?v=3&s=460'
+        title: '【天空之城】阳春三月 携死党．奔赴女儿国（完结）',
+        subTitle: '天空之城',
+        img: 'img/banners/2.jpg'
     }, {
         id: 2,
-        name: 'Andrew Jostlin',
-        lastText: 'Did you get the ice cream?',
-        face: 'https://pbs.twimg.com/profile_images/491274378181488640/Tti0fFVJ.jpeg'
+        title: '#消夏计划#I TAIWAN臺灣向南走向北走',
+        subTitle: 'ar_vinny ',
+        img: 'img/banners/3.jpg'
     }, {
         id: 3,
-        name: 'Adam Bradleyson',
-        lastText: 'I should buy a boat',
-        face: 'https://pbs.twimg.com/profile_images/479090794058379264/84TKj_qa.jpeg'
-    }, {
-        id: 4,
-        name: 'Perry Governor',
-        lastText: 'Look at my mukluks!',
-        face: 'https://pbs.twimg.com/profile_images/491995398135767040/ie2Z_V6e.jpeg'
+        title: '沙丁鱼大迁徙－狂野南非 海盗王.基德作品',
+        subTitle: '海盗王基德',
+        img: 'img/banners/4.jpg'
     }];
 
     return {
